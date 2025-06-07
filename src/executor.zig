62fcec2484db949c92572e5297bf5956399fe1f1
satkinson <Steven_Atkinson@intuit.com>
2025-06-07 11:00:08 -0700

    Pull execution out of the Ast

---
+++ src/executor.zig
@@ -0,0 +1,43 @@
+const std = @import("std");
+const yash = @import("yash.zig");
+const exec = @import("exec.zig");
+const builtins = @import("builtins.zig");
+const Ast = @import("ast.zig").Ast;
+
+pub const Executor = struct {
+    allocator: std.mem.Allocator,
+
+    pub fn init(allocator: std.mem.Allocator) Executor {
+        return Executor{
+            .allocator = allocator,
+        };
+    }
+
+    pub fn execute(self: *Executor, ast: *const Ast) !u8 {
+        var last_exit_code: u8 = 0;
+        for (ast.statements) |stmt| {
+            last_exit_code = try self.executeStatement(&stmt);
+        }
+        return last_exit_code;
+    }
+
+    fn executeStatement(self: *Executor, statement: *const Ast.Statement) !u8 {
+        return switch (statement.*) {
+            .Command => |cmd| try self.executeCommand(cmd),
+        };
+    }
+
+    fn executeCommand(self: *Executor, cmd: Ast.CommandExpr) !u8 {
+        if (cmd.name.len == 0)
+            return yash.ShellError.ParseError; // TODO: fix this
+
+        const maybeBuiltin = builtins.findBuiltin(cmd.name);
+        if (maybeBuiltin) |func| {
+            func(self.allocator, cmd.args);
+            return 0;
+        }
+        // TODO: is it an alias, or a built-in
+
+        return try exec.spawn(self.allocator, cmd.name, cmd.args);
+    }
+}; 
--- src/repl.zig
+++ src/repl.zig
@@ -4,10 +4,13 @@ const exec = @import("exec.zig");
 const yash = @import("ast.zig");
 const Parser = @import("parser.zig").Parser;
 const Lexer = @import("lexer.zig").Lexer;
+const Executor = @import("executor.zig").Executor;
 
 const max_line_size = 1024;
 
 pub fn run(allocator: std.mem.Allocator) !void {
+    var executor = Executor.init(allocator);
+    
     while (true) {
         try prompt.display();
         const line = try get_input_line(allocator);
@@ -17,13 +20,9 @@ pub fn run(allocator: std.mem.Allocator) !void {
         const MyParser = Parser(@TypeOf(lexer));
         var parser = try MyParser.init(allocator, &lexer);
         var ast = try parser.parse();
-        // TODO: execute the ast
         defer ast.deinit(allocator);
 
-        // TODO: now parse the line to break it into a series of commands
-        // what's the best way to handle &&, ||, subshells
-        // should get back a tree to execute
-        const exit_code: u8 = try ast.execute(allocator);
+        const exit_code: u8 = try executor.execute(&ast);
         // TODO: keep track of last error
         _ = exit_code;
     }
