3d6920d9c7a3bf42ee872e26d2342963083e0e68
Steven Atkinson <atkinson.steven@gmail.com>
2025-06-16 19:11:31 -0700

    Removed explicit flush from print

--- src/yash.zig
+++ src/yash.zig
@@ -7,17 +7,13 @@ pub const ShellError = error{
     ParseError,
 };
 
-/// Print to stdout with immediate flush. Panics on failure.
+/// Print to stdout. Panics on failure.
 pub fn print(comptime fmt: []const u8, args: anytype) void {
     const stdout = std.io.getStdOut().writer();
     stdout.print(fmt, args) catch |err| {
         std.debug.print("yash: fatal error writing to stdout: {}\n", .{err});
         std.debug.panic("stdout failure is unrecoverable for a shell\n", .{});
     };
-    //std.io.getStdOut().sync() catch |err| {
-        //std.debug.print("yash: fatal error flushing stdout: {}\n", .{err});
-        //std.debug.panic("stdout failure is unrecoverable for a shell\n", .{});
-    //};
 }
 
 fn isExecutable(path: []const u8) bool {
