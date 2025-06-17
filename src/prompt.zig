c0f16b9a4b31df81a21d171bca84f6ea15497510
Steven Atkinson <atkinson.steven@gmail.com>
2025-06-16 19:12:50 -0700

    Check tty before printing prompt

--- src/prompt.zig
+++ src/prompt.zig
@@ -1,7 +1,11 @@
 const yash = @import("yash.zig");
+const std = @import("std");
 
 const default_prompt = "$ ";
 
 pub fn display() void {
-    yash.print(default_prompt, .{});
+    const stdin = std.io.getStdIn();
+    if (stdin.isTty()) {
+        yash.print(default_prompt, .{});
+    }
 }
